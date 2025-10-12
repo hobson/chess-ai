81d8ae509f31ad0fb2925ba99057ba48275bd2eb
hobs <git@totalgood.com>
2025-10-12 16:01:52 -0700

    runtime find assets/

--- src/chess_minimax/graphics.py
+++ src/chess_minimax/graphics.py
@@ -1,56 +1,61 @@
 import pygame
 from .ChessPiece import *
-from .Computer import get_random_move, get_ai_move
+from .Computer import get_ai_move  # , get_random_move
+from pathlib import Path
 
-dark_block = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/square brown dark_png_shadow_128px.png')
-light_block = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/square brown light_png_shadow_128px.png')
+ASSETS_DIR = Path(__file__).parent.parent / 'assets'
+
+dark_block = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/square brown dark_png_shadow_128px.png')
+light_block = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/square brown light_png_shadow_128px.png')
 dark_block = pygame.transform.scale(dark_block, (75, 75))
 light_block = pygame.transform.scale(light_block, (75, 75))
 
-whitePawn = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_pawn_png_shadow_128px.png')
+whitePawn = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_pawn_png_shadow_128px.png')
 whitePawn = pygame.transform.scale(whitePawn, (75, 75))
-whiteRook = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_rook_png_shadow_128px.png')
+whiteRook = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_rook_png_shadow_128px.png')
 whiteRook = pygame.transform.scale(whiteRook, (75, 75))
-whiteBishop = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_bishop_png_shadow_128px.png')
+whiteBishop = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_bishop_png_shadow_128px.png')
 whiteBishop = pygame.transform.scale(whiteBishop, (75, 75))
-whiteKnight = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_knight_png_shadow_128px.png')
+whiteKnight = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_knight_png_shadow_128px.png')
 whiteKnight = pygame.transform.scale(whiteKnight, (75, 75))
-whiteKing = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_king_png_shadow_128px.png')
+whiteKing = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_king_png_shadow_128px.png')
 whiteKing = pygame.transform.scale(whiteKing, (75, 75))
-whiteQueen = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/w_queen_png_shadow_128px.png')
+whiteQueen = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/w_queen_png_shadow_128px.png')
 whiteQueen = pygame.transform.scale(whiteQueen, (75, 75))
 
-blackPawn = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_pawn_png_shadow_128px.png')
+blackPawn = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_pawn_png_shadow_128px.png')
 blackPawn = pygame.transform.scale(blackPawn, (75, 75))
-blackRook = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_rook_png_shadow_128px.png')
+blackRook = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_rook_png_shadow_128px.png')
 blackRook = pygame.transform.scale(blackRook, (75, 75))
-blackBishop = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_bishop_png_shadow_128px.png')
+blackBishop = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_bishop_png_shadow_128px.png')
 blackBishop = pygame.transform.scale(blackBishop, (75, 75))
-blackKnight = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_knight_png_shadow_128px.png')
+blackKnight = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_knight_png_shadow_128px.png')
 blackKnight = pygame.transform.scale(blackKnight, (75, 75))
-blackKing = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_king_png_shadow_128px.png')
+blackKing = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_king_png_shadow_128px.png')
 blackKing = pygame.transform.scale(blackKing, (75, 75))
-blackQueen = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/b_queen_png_shadow_128px.png')
+blackQueen = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/b_queen_png_shadow_128px.png')
 blackQueen = pygame.transform.scale(blackQueen, (75, 75))
 
-highlight_block = pygame.image.load('assets/JohnPablok Cburnett Chess set/128px/highlight_128px.png')
+highlight_block = pygame.image.load(ASSETS_DIR / 'JohnPablok Cburnett Chess set/128px/highlight_128px.png')
 highlight_block = pygame.transform.scale(highlight_block, (75, 75))
 
 screen = None
 pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 30)
 
+
 def initialize():
     global screen
     pygame.init()
     pygame.display.set_caption('Chess!')
-    icon = pygame.image.load('assets/icon.png')
+    icon = pygame.image.load(ASSETS_DIR / 'icon.png')
     pygame.display.set_icon(icon)
     screen = pygame.display.set_mode((600, 650))
     screen.fill((0, 0, 0))
 
+
 def draw_background(board):
-    
+
     block_x = 0
     for i in range(4):
         block_y = 0
@@ -72,6 +77,7 @@ def draw_background(board):
         step_x = 0
         step_y -= 75
     pygame.display.update()
+
 
 def draw_text(text):
     s = pygame.Surface((400, 50))
@@ -139,7 +145,7 @@ def start(board):
                             if board.ai:
                                 get_ai_move(board)
                                 draw_background(board)
-                                    
+
                         if board.white_won():
                             game_over = True
                             game_over_txt = 'WHITE WINS!'
